4bafff2f45ea51c41f80bcd7b7c975ed4fcd5efe
Lixing <lexerzhang@gmail.com>
2020-12-07 18:49:51 +0100

    Edit: First sudoku attempt. Not tested.

---
+++ Logic_Agents/Sudoku_Solving_Agent/function.py
@@ -0,0 +1,111 @@
+from utils import *
+
+
+# `grid` is defined in the test code scope as the following:
+# (note: changing the value here will _not_ change the test code)
+# grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+
+def grid_values(grid):
+    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
+    """
+    empty_filler = "123456789"
+    grid_list = list(grid)
+    for i in range(len(grid)):
+        if grid[i] == '.':
+            grid_list[i] = empty_filler
+    sudoku_dict = dict(zip(boxes, grid_list))
+    return sudoku_dict
+
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    values_new = values.copy()
+    for key in values.keys():
+        for peer in peers[key]:
+            peer_val = values[peer]
+            if len(peer_val) == 1:
+                values_new[key] = values_new[key].replace(peer_val, '')
+    return values_new
+
+
+def only_choice(values):
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    values_new = values.copy()
+    for key in peers.keys():
+        box_val_set = set(values[key])
+        if len(box_val_set) > 1:
+            for peer in peers[key]:
+                box_val_set -= set(values[peer])
+        if len(box_val_set) == 1:
+            values_new[key] = ''.join(box_val_set)
+    return values_new
+
+
+def reduce_puzzle(values):
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+
+def search(values):
+    "Using depth-first search and propagation, create a search tree and solve the sudoku."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
+
+if __name__ == "__main__":
+    grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
+    sudoku_dict = eliminate(grid_values(grid))
+    display(sudoku_dict)
